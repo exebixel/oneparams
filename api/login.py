e7732fa88885ead27b29e494512f5600964d8ce0
exebixel <ezequielnat7@gmail.com>
2020-06-13 15:52:33 -0300

    refactory login

--- api/login.py
+++ api/login.py
@@ -19,38 +19,24 @@ class login(base_api):
         }
 
         print("logging in")
-        response = requests.post(
-            "{0}/ologin".format(self.api_url),
-            data = json.dumps(dados),
-            headers = self.header
+        response = self.post(
+            "/ologin",
+            data = dados
         )
 
-        if response.status_code == 200:
+        if self.status_ok(response):
             content = json.loads(response.content)
             access_token =  content["data"]["access_token"]
             print("successful login")
             super().update_token(access_token)
 
-        else:
-            print("login erro!")
-            print(f'Erro code: {response.status_code}')
-            print(response.content)
-            sys.exit(0)
-
-
     def empresa_id(self, name):
 
         name = quote(name)
-        response = requests.get(
-            "{0}/OMobilidades/PesquisarEmpresaPorNome/{1}".format(self.api_url, name),
-            headers = self.header
+        response = self.get(
+            "/OMobilidades/PesquisarEmpresaPorNome/{0}".format(name),
         )
-
-        if response.status_code != 200:
-            print("Error finding company")
-            print(f'Erro code: {response.status_code}')
-            print(response.content)
-            sys.exit(0)
+        self.status_ok(response)
 
         content = json.loads(response.content)
         if len(content) != 1:
@@ -68,20 +54,13 @@ class login(base_api):
     def filial_id(self, empresa_id, name):
 
         name = quote(name)
-        response = requests.get(
-            "{0}/OMobilidades/PesquisarFilialPorNome/{1}/{2}".format(
-                self.api_url,
+        response = self.get(
+            "/OMobilidades/PesquisarFilialPorNome/{0}/{1}".format(
                 empresa_id,
                 name
-            ),
-            headers = self.header
+            )
         )
-
-        if response.status_code != 200:
-            print("Error finding branch")
-            print(f'Erro code: {response.status_code}')
-            print(response.content)
-            sys.exit(0)
+        self.status_ok(response)
 
         content = json.loads(response.content)
         if len(content) != 1:
