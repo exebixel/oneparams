b86f6c36101225e4379f5034b6b153ec4c319137
exebixel <ezequielnat7@gmail.com>
2020-07-11 14:17:51 -0300

    update excel/comissao

--- excel/comissao.py
+++ excel/comissao.py
@@ -4,10 +4,18 @@ from utils import *
 def comissao(book):
     ex = excel(book, "servico")
 
-    ex.add_column(key="ServicosNome", name="nome")
-    ex.add_column(key="profissional", name="profissionais")
+    ex.add_column(key="servico", name="nome")
+    ex.add_column(key="cols", name="profissionais")
 
     for row in range(2, ex.nrows):
         data = ex.data_row(row)
-        data["profissional"] = get_names(data["profissional"])
-        print(data)
+        data["cols"] = get_names(data["cols"])
+
+        proc_data = []
+        for i in data["profissional"]:
+            proc_data.append({
+                "cols": i,
+                "servico": data["ServicosNome"],
+                "ativo": True
+            })
+        print(proc_data)
