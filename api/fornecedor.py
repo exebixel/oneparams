46ce42226c61cd66327961db158eb0a7536a249c
exebixel <ezequielnat7@gmail.com>
2020-07-17 12:11:19 -0300

    code style api/forncedor

--- api/fornecedor.py
+++ api/fornecedor.py
@@ -1,14 +1,25 @@
-from api.base import base_api
-from utils import *
+"""
+modulo auxiliar, sua principal função é retornar um fornecedor com determinado nome
+"""
 import json
+from api.base import BaseApi
+from utils import create_cel, create_email
 
-class fornecedor(base_api):
+class Fornecedor(BaseApi):
+    """
+    classe de gerenciamento de fornecedores da one,
+    sua principal função é criar e pesquisar fornecedores
+    """
 
     def __init__(self):
         self.__fornecedores = []
         self.all_fornecedores()
 
     def all_fornecedores(self):
+        """
+        Pega todos os fornecedores cadastrados no sistema,
+        e preenche o atributo self.__fornecedores com nome e id
+        """
         print("researching supplier")
         response = self.get("/CliForCols/ListaDetalhesFornecedores")
         self.status_ok(response)
@@ -21,16 +32,33 @@ class fornecedor(base_api):
             })
 
     def get_id(self, nome):
+        """
+        Retorna o id de um fornecedor,
+        o nome do fornecedor tem que ser exatamente igual,
+        se não encontrar o id do fornecedor, retorna None
+        """
         for i in self.__fornecedores:
             if i["nome"] == nome:
                 return i["id"]
         return None
 
     def create(self, nome):
+        """
+        Cria um fornecedor, 
+        Dados de criação padrão:
+        data={
+            "ativoFornecedor": "true",
+            "flagCliente": "false",
+            "flagColaborador": "false",
+            "email": gerado aleatoriamente,
+            "celular": gerado aleatoriamente,
+            "nomeCompleto": nome (parâmetro)
+        }
+        """
         print("creating {} supplier".format(nome))
         response = self.post(
             "/OCliForColsUsuarioPerfil/CreateFornecedores",
-            data = {
+            data={
                 "ativoFornecedor": "true",
                 "flagCliente": "false",
                 "flagColaborador": "false",
@@ -48,7 +76,11 @@ class fornecedor(base_api):
         return content["data"]
 
     def get_for(self, nome):
+        """
+        Retorna o id de um fornecedor com base em seu nome,
+        se o fornecedor não existir, ele será criado
+        """
         for_id = self.get_id(nome)
-        if for_id == None:
+        if for_id is None:
             for_id = self.create(nome)
         return for_id
