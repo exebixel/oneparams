b065d4059b4d6d333ebcde89d14e424ec66b53a2
exebixel <ezequielnat7@gmail.com>
2020-07-31 13:13:53 -0300

    code style args.py

--- args.py
+++ args.py
@@ -1,15 +1,15 @@
-import sys, argparse
-
 def parse_base(parser):
-    parser.add_argument('worksheet',
-                        help= "Parameterization worksheet path")
-    parser.add_argument("--login", "-l", required=True,
-                        metavar="EMAIL", help= "Login email")
-    parser.add_argument("--empresa", "-e", required=True,
-                        help= "Company Name")
-    parser.add_argument("--filial", "-f",
-                        help= "Branch name (if any)")
-    parser.add_argument("--password", "-p", default="123456",
-                        help= "Access password (default = 123456)")
+    parser.add_argument('worksheet', help="Parameterization worksheet path")
+    parser.add_argument("--login",
+                        "-l",
+                        required=True,
+                        metavar="EMAIL",
+                        help="Login email")
+    parser.add_argument("--empresa", "-e", required=True, help="Company Name")
+    parser.add_argument("--filial", "-f", help="Branch name (if any)")
+    parser.add_argument("--password",
+                        "-p",
+                        default="123456",
+                        help="Access password (default = 123456)")
 
     return parser
