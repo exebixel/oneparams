a675489d351a78913b858cfd3ba7079ed09b465b
exebixel <ezequielnat7@gmail.com>
2020-08-30 10:02:48 -0300

    change url base

--- oneparams/api/base.py
+++ oneparams/api/base.py
@@ -14,7 +14,7 @@ class BaseApi:
     header = {'Content-Type': 'application/json'}
 
     def __init_subclass__(cls):
-        cls.api_url = "https://oneapinovo.azurewebsites.net/api"
+        cls.api_url = "https://oneapilite.azurewebsites.net/api"
 
     def update_token(self, token):
         """
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     entry_points={
         'console_scripts': ['one = oneparams.one:main'],
     },
-    version="0.1.18",
+    version="0.1.20",
     description="One System Parametrizer",
     long_description=long_description,
     long_description_content_type="text/markdown",
