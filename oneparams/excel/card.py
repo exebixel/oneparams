01b8a0b735567fa042b5aac607ff859902464e55
exebixel <ezequielnat7@gmail.com>
2021-01-16 19:03:51 -0300

    updated excel.cards to new excel.excel functions

--- oneparams/excel/card.py
+++ oneparams/excel/card.py
@@ -6,6 +6,7 @@ from oneparams.utils import card_type, deemphasize
 
 
 def cards(book, reset=False):
+    print("analyzing spreadsheet")
 
     ex = Excel(book=book, sheet_name="cart")
 
@@ -16,17 +17,16 @@ def cards(book, reset=False):
                   name="cobrada",
                   default=0,
                   types="float")
-    ex.add_column(key="operadoraCartao", name="operadora", default="Padrão")
-    ex.add_column(key="contas",
+    ex.add_column(key="operadoraCartaoId", name="operadora", default="Padrão")
+    ex.add_column(key="contasId",
                   name="conta",
                   required=False,
                   default="conta corrente")
+    ex.clean_columns()
 
-    one = apiCard()
-
-    print("analyzing spreadsheet")
     data_all = ex.data_all(check_row=checks)
 
+    one = apiCard()
     if reset:
         one.delete_all()
         operadora = Operadora()
@@ -36,30 +36,29 @@ def cards(book, reset=False):
         one.diff_item(row)
 
 
-def checks(row, data, previous):
+def checks(row, data):
     erros = False
     conta = Conta()
 
-    data["contasId"] = conta.return_id(data["contas"])
-    data.pop("contas")
+    data["contasId"] = conta.return_id(data["contasId"])
     if data["contasId"] == None:
         print(
-            f'ERROR! in line {row + 1}: Card {data["descricao"]} account not found'
+            f'ERROR! in line {row}: Card {data["descricao"]} account not found'
         )
         erros = True
 
     if data["descricao"] is None:
-        print("ERROR! in line {}: Name cannot be null".format(row + 1))
+        print("ERROR! in line {}: Name cannot be null".format(row))
         erros = True
     if len(data["descricao"]) > 100:
         print(
-            f'ERROR! in line {row + 1}: Card {data["descricao"]} name size {len(data["descricao"])} > 100'
+            f'ERROR! in line {row}: Card {data["descricao"]} name size {len(data["descricao"])} > 100'
         )
         erros = True
 
-    if len(data["operadoraCartao"]) > 50:
+    if len(data["operadoraCartaoId"]) > 50:
         print(
-            f'ERROR! in line {row + 1}: Card {data["descricao"]} card operator size {len(data["operadoraCartao"])} > 50'
+            f'ERROR! in line {row}: Card {data["descricao"]} card operator size {len(data["operadoraCartaoId"])} > 50'
         )
         erros = True
 
@@ -67,30 +66,30 @@ def checks(row, data, previous):
         data["debito_Credito"] = card_type(data["debito_Credito"])
     except TypeError as exp:
         if data["debito_Credito"] is not None:
-            print(f'ERROR! in line {row + 1}: Card {data["descricao"]} {exp}')
+            print(f'ERROR! in line {row}: Card {data["descricao"]} {exp}')
             erros = True
 
         data["debito_Credito"] = "CD"
 
-    for prev in previous:
-        descricao = deemphasize(data["descricao"])
-        prev_descricao = deemphasize(prev["data"]["descricao"])
-        if (descricao == prev_descricao
-                and prev["data"]["debito_Credito"] in data["debito_Credito"]):
-            print(
-                f'ERROR! in lines {row + 1} and {prev["row"] + 1}: Card {data["descricao"]} is duplicated'
-            )
-            erros = True
+    # for prev in previous:
+    #     descricao = deemphasize(data["descricao"])
+    #     prev_descricao = deemphasize(prev["data"]["descricao"])
+    #     if (descricao == prev_descricao
+    #             and prev["data"]["debito_Credito"] in data["debito_Credito"]):
+    #         print(
+    #             f'ERROR! in lines {row} and {prev["row"] + 1}: Card {data["descricao"]} is duplicated'
+    #         )
+    #         erros = True
 
     if erros:
         raise Exception
 
-    if data["debito_Credito"] == "CD":
-        data2 = {}
-        for i, j in data.items():
-            data2[i] = j
+    # if data["debito_Credito"] == "CD":
+    #     data2 = {}
+    #     for i, j in data.items():
+    #         data2[i] = j
 
-        data["debito_Credito"] = "C"
-        data2["debito_Credito"] = "D"
-        return [data, data2]
+    #     data["debito_Credito"] = "C"
+    #     data2["debito_Credito"] = "D"
+    #     return [data, data2]
     return data
