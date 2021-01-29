671e23dd6b46deb2310ccc4365b91d6ada3eb5a9
exebixel <ezequielnat7@gmail.com>
2021-01-29 00:39:44 -0300

    fixed improper service check

--- oneparams/excel/comissao.py
+++ oneparams/excel/comissao.py
@@ -4,83 +4,82 @@ from oneparams.excel.excel import Excel
 from oneparams.utils import get_names
 
 
-def comissao(book, reset=False):
-    print("analyzing spreadsheet")
+class Comissao():
+    def __init__(self, book, reset=False):
+        self.erros = False
+        self.comissao(book=book, reset=reset)
 
-    ex = Excel(book, "servico")
-    ex.add_column(key="servico", name="nome")
-    ex.add_column(key="servId",
-                  name="servico id",
-                  required=False,
-                  types="float",
-                  default=0)
-    ex.add_column(key="ServicoValorComissao",
-                  name="comissao",
-                  default=0,
-                  types="float")
-    ex.add_column(key="cols", name="profissionais")
-    ex.add_column(key="colsId", required=False, types="float", name="colsId")
-    ex.clean_columns()
-    ex.add_row_column()
+    def comissao(self, book, reset=False):
+        print("analyzing spreadsheet")
 
-    one = Commission()
+        ex = Excel(book, "servico")
+        ex.add_column(key="servico", name="nome")
+        ex.add_column(key="ServicoValorComissao",
+                      name="comissao",
+                      default=0,
+                      types="float")
+        ex.add_column(key="cols", name="profissionais")
+        ex.clean_columns()
+        ex.add_row_column()
 
-    data_all = ex.data_all(check_final=checks_comm)
+        one = Commission()
 
-    if reset:
-        one.delete_all()
+        data_all = ex.data_all(check_final=self.checks_comm)
 
-    for row in data_all:
-        one.comissao(row)
+        if reset:
+            one.delete_all()
 
+        for row in data_all:
+            one.comissao(row)
 
-erros = False
+    def checks_comm(self, self_excel, data):
+        data = data.loc[data["cols"].notnull()]
+        data = data.apply(self.get_serv_id, axis=1)
 
+        final_data = pd.DataFrame()
+        for i in data.iterrows():
+            final_data = final_data.append(self.cols_names_to_id(i[1]))
 
-def checks_comm(self, data):
-    api = Commission()
+        if self.erros:
+            raise Exception
+        return final_data
 
-    data = data.loc[data["cols"].notnull()]
-    data = data.apply(get_serv_id, axis=1)
+    def get_serv_id(self, data):
+        api = Commission()
+        data["servId"] = api.serv.item_id({"descricao": data["servico"]})
+        if data["servId"] == 0:
+            print("ERROR!! in line {}: Service {} not found".format(
+                data["row"], data["servico"]))
+            self.erros = True
+        return data
 
-    final_data = pd.DataFrame()
-    for i in data.iterrows():
-        final_data = final_data.append(cols_names_to_id(i[1]))
+    def cols_names_to_id(self, data):
+        cols = get_names(data["cols"])
 
-    if erros:
-        raise Exception
-    return final_data
+        api = Commission()
 
+        ids = []
+        for i in cols:
+            try:
+                ids.append(api.cols.search_item_by_name(i))
+            except ValueError as exp:
+                print("ERROR!! in line {}: {}".format(data["row"], exp))
+                self.erros = True
 
-def get_serv_id(data):
-    api = Commission()
-    data["servId"] = api.serv.item_id({"descricao": data["servico"]})
-    if data["servId"] == 0:
-        print("ERROR!! in line {}: Service {} not found".format(
-            data["row"], data["servico"]))
-        erros = True
-    return data
-
-
-def cols_names_to_id(data):
-    cols = get_names(data["cols"])
-
-    api = Commission()
-
-    ids = []
-    for i in cols:
-        try:
-            ids.append(api.cols.search_item_by_name(i))
-        except ValueError as exp:
-            print("ERROR!! in line {}: {}".format(data["row"], exp))
-            erros = True
-
-    return pd.DataFrame({
-        "servico": data["servico"],
-        "servId": data["servId"],
-        "cols": cols,
-        "colsId": ids,
-        "ServicoValorComissao": data["ServicoValorComissao"],
-        "ativo": True,
-        "row": data["row"]
-    })
+        if not self.erros:
+            return pd.DataFrame({
+                "servico":
+                data["servico"],
+                "servId":
+                data["servId"],
+                "cols":
+                cols,
+                "colsId":
+                ids,
+                "ServicoValorComissao":
+                data["ServicoValorComissao"],
+                "ativo":
+                True,
+                "row":
+                data["row"]
+            })
