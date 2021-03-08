a649f8db9a107191956ef96fb67220f343237bc9
exebixel <ezequielnat7@gmail.com>
2021-03-08 20:19:26 -0300

    fix requirements

--- oneparams/excel/excel.py
+++ oneparams/excel/excel.py
@@ -3,7 +3,6 @@ import sys
 from datetime import time
 
 import pandas as pd
-import xlrd
 from oneparams.utils import get_bool, get_float, get_time, string_normalize
 
 
--- setup.py
+++ setup.py
@@ -22,15 +22,14 @@ setuptools.setup(
     entry_points={
         'console_scripts': ['one = oneparams.one:main'],
     },
-    version="0.2.5",
+    version="0.2.6",
     description="One System Parametrizer",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="exebixel",
     author_email="ezequielnat7@gmail.com",
     url="https://github.com/exebixel/oneparams",
-    install_requires=["requests", "pandas", "odspy",
-                      "urllib3"],
+    install_requires=["requests", "pandas", "urllib3"],
     python_requires=">=3.6",
     classifiers=[
         "Programming Language :: Python",
