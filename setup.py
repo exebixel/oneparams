e9b2c6f2ec4281503d50fa6cbdb279062753ce2a
exebixel <ezequielnat7@gmail.com>
2021-03-16 13:23:48 -0300

    update version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     entry_points={
         'console_scripts': ['one = oneparams.one:main'],
     },
-    version="0.2.7",
+    version="0.2.8",
     description="One System Parametrizer",
     long_description=long_description,
     long_description_content_type="text/markdown",
