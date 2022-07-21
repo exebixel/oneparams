cee93dc18f8ecd7e537ab3348761163ea2038a95
exebixel <ezequielnat7@gmail.com>
2022-07-21 11:10:55 -0300

    added dict to check if a value exist fast in clis module

--- oneparams/api/client.py
+++ oneparams/api/client.py
@@ -7,9 +7,10 @@ from oneparams.utils import create_email, deemphasize
 
 
 class ApiCliente(BaseDiff):
-    items = {}
-    list_details = {}
-    first_get = False
+    items: dict = {}
+    list_details: dict = {}
+    list_search: dict[str, dict] = {}
+    first_get: bool = False
 
     def __init__(self):
         self.url_get_all = "/CliForCols/ListaDetalhesClientes"
@@ -32,7 +33,7 @@ class ApiCliente(BaseDiff):
             self.get_all()
             ApiCliente.first_get = True
 
-    def get_all(self) -> None:
+    def get_all(self) -> dict:
         print(f"researching {self.item_name}")
         ApiCliente.items = {}
 
@@ -40,6 +41,8 @@ class ApiCliente(BaseDiff):
         self.status_ok(response)
         content = json.loads(response.content)
 
+        self.list_search[self.key_name] = {}
+        self.list_search["email"] = {}
         for i in content:
             ApiCliente.items[i["cliForColsId"]] = {
                 self.key_id: i["cliForColsId"],
@@ -47,6 +50,10 @@ class ApiCliente(BaseDiff):
                 self.key_name: i[self.key_name],
                 "email": i["email"]
             }
+            self.list_search[self.key_name][i[
+                self.key_name]] = i["cliForColsId"]
+            self.list_search["email"][deemphasize(
+                i["email"])] = i["cliForColsId"]
 
         response = self.get(f'{self.url_get_all}/false')
         content = json.loads(response.content)
@@ -58,6 +65,12 @@ class ApiCliente(BaseDiff):
                 self.key_name: i[self.key_name],
                 "email": i["email"]
             }
+            self.list_search[self.key_name][i[
+                self.key_name]] = i["cliForColsId"]
+            self.list_search["email"][deemphasize(
+                i["email"])] = i["cliForColsId"]
+
+        return ApiCliente.items
 
     def add_item(self, data: dict, response: dict) -> int:
         data = {
@@ -65,39 +78,53 @@ class ApiCliente(BaseDiff):
             self.key_name: data[self.key_name],
             "email": data["email"]
         }
+
+        self.list_search[self.key_name][data[self.key_name]] = response["data"]
+        self.list_search["email"][deemphasize(
+            data["email"])] = response["data"]
+
         return super().add_item(data, response)
 
-    def equals(self, data: dict) -> None:
+    def equals(self, data: dict) -> bool:
         if pd.isnull(data["email"]):
             data.pop("email")
         if pd.isnull(data["celular"]):
             data.pop("celular")
         return super().equals(data)
 
-    def create(self, data: dict) -> None:
+    def create(self, data: dict) -> int:
         if pd.isnull(data["email"]):
             data["email"] = create_email()
         if pd.isnull(data["celular"]):
             data["celular"] = "00000000"
-        super().create(data)
+        return super().create(data)
 
     def update(self, data: dict) -> None:
+        details = self.details(data[self.key_id])
+
         if "email" not in data.keys():
-            data["email"] = self.details(data[self.key_id])["email"]
+            data["email"] = details["email"]
         if "celular" not in data.keys():
-            data["celular"] = self.details(data[self.key_id])["celular"]
+            data["celular"] = details["celular"]
+
+        self.list_search[self.key_name].pop(details[self.key_name])
+        self.list_search["email"].pop(deemphasize(details["email"]))
         return super().update(data)
 
-    def item_id(self, data: dict) -> None:
+    def item_id(self, data: dict) -> int:
         name = data[self.key_name]
         email = deemphasize(data["email"])
 
-        for key, item in self.items.items():
-            existent_name = item[self.key_name]
-            existent_email = deemphasize(item["email"]).strip()
+        try:
+            return self.list_search[self.key_name][name]
+        except KeyError:
+            pass
 
-            if (existent_name == name or existent_email == email):
-                return key
+        try:
+            return self.list_search["email"][email]
+        except KeyError:
+            pass
+
         return 0
 
     def name_to_id(self, data: dict) -> dict:
